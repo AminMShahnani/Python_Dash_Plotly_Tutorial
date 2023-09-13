a82567a9a9542c68b73f1aeb6895f3aae90cfcc7
aminmshahnani <mshahnani.amin@gmail.com>
2023-09-13 17:46:44 +0330

    Create 10.Customizing-With-Inline-CSS.py

---
+++ Codes/10.Customizing-With-Inline-CSS.py
@@ -0,0 +1,33 @@
+from dash import Dash, html, dcc
+import pandas as pd
+
+soccer=pd.read_csv('../CSV_Files/fifasoccerplayers.csv')
+
+app=Dash()
+
+app.layout=html.Div([
+    html.H1('Soccer Players dashboard', 
+            style={'textAlign' : 'center',
+                   'fontFamily': 'fantasy',
+                   'fontSize': 50,
+                   'color': 'blue'}
+            ),
+    html.P(['Source:  ',
+        html.A(
+            'Sofifa',
+            href='https://sofifa.com',
+            target='_blank'
+        )],
+        style={'border':'solid'}),
+    html.Label('Player name: '),
+    dcc.Dropdown(
+        options=soccer['long_name'].unique(),
+        value=soccer['long_name'].unique()[8],
+        style={'backgroundColor':'lightblue'}
+    )
+],
+style={'padding':100, 'border':'solid'}
+)
+
+if __name__ == '__main__'   :
+    app.run_server(debug=True)
