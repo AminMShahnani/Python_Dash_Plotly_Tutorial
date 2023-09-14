6b697a73fcd8af7757f6866d08ce8f62d5f4a83f
aminmshahnani <mshahnani.amin@gmail.com>
2023-09-14 14:01:37 +0330

    Update 18.Adding-Callback-Update-The-Map.py

--- Codes/18.Adding-Callback-Update-The-Map.py
+++ Codes/18.Adding-Callback-Update-The-Map.py
@@ -10,17 +10,6 @@ year_min = electricity['Year'].min()
 year_max = electricity['Year'].max()
 
 
-avg_price_electricity = electricity \
-                        .groupby('US_State')['Residential Price'] \
-                        .mean() \
-                        .reset_index() 
-map_fig = px.choropleth(avg_price_electricity,
-                        locations='US_State', 
-                        locationmode='USA-states',
-                        color='Residential Price',
-                        scope='usa',
-                        color_continuous_midpoint=1)
-
 app = Dash(external_stylesheets=[dbc.themes.SOLAR])
 
 app.layout= html.Div([
@@ -30,10 +19,34 @@ app.layout= html.Div([
                     max=year_max,
                     value=[year_min, year_max],
                     marks={i:str(i) for i in range(year_min, year_max+1)}),
-    dcc.Graph(id='map-graph', figure=map_fig),
+    dcc.Graph(id='map-graph'),
     dash_table.DataTable(id='price-info',
                          data=electricity.to_dict('records'))
 ])
+
+@app.callback(
+    Output('map-graph', 'figure'),
+    Input('year-slider', 'value')
+)
+def update_map_graph(selected_years):
+    filtered_electricity=electricity[
+        (electricity['Year']>=selected_years[0]) &
+        (electricity['Year']<=selected_years[1])
+    ]
+
+    avg_price_electricity = filtered_electricity \
+                            .groupby('US_State')['Residential Price'] \
+                            .mean() \
+                            .reset_index() 
+    map_fig = px.choropleth(avg_price_electricity,
+                            locations='US_State', 
+                            locationmode='USA-states',
+                            color='Residential Price',
+                            scope='usa',
+                            color_continuous_midpoint=5)
+    return map_fig
+
+
 
 if __name__ == '__main__' :
     app.run_server(debug=True)
